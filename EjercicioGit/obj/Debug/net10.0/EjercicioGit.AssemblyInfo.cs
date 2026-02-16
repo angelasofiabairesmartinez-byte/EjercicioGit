e5edf608bc4318c30e4d0cb651199bb09d899fb9
Sofía Baires <angelasofiabairesmartinez@gmail.com>
2026-02-15 19:44:59 -0600

    cuarto commit

--- EjercicioGit/obj/Debug/net10.0/EjercicioGit.AssemblyInfo.cs
+++ EjercicioGit/obj/Debug/net10.0/EjercicioGit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjercicioGit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9bd2c1ff07f8224cb981ae5b4575d0c358e28c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+786268f43ba5261822b32b14cb3f246c3003ef8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjercicioGit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjercicioGit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
